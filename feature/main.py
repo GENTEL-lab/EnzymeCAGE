34cdacdc071f690d20ff46377ff779036b583435
yliust <yliupe@connect.ust.hk>
2025-01-20 12:35:21 +0800

    update

--- feature/main.py
+++ feature/main.py
@@ -344,16 +344,16 @@ def calc_reacting_center(data_path, save_dir, append=True):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_path', type=str, default='./dataset/testing/test.csv')
-    parser.add_argument('--alphafill_result_dir', type=str, default='./dataset/testing/alphafill_results')
+    parser.add_argument('--data_path', type=str, required=True)
+    parser.add_argument('--alphafill_result_dir', type=str, default=None)
     parser.add_argument('--pocket_dir', type=str, help='If you already have pocket data, you can specify the directory here')
     parser.add_argument('--skip_calc_mol_conformation', action='store_true')
     args = parser.parse_args()
     
     # alphafill_result_dir = os.path.join(os.path.dirname(args.data_path), 'alphafill_results')
     alphafill_result_dir = args.alphafill_result_dir
-    if not os.path.exists(alphafill_result_dir):
-        raise FileNotFoundError(f'alphafill result not found in {alphafill_result_dir}')
+    # if not os.path.exists(alphafill_result_dir):
+    #     raise FileNotFoundError(f'alphafill result not found in {alphafill_result_dir}')
     
     if not args.pocket_dir:
         pocket_dir = os.path.join(os.path.dirname(args.data_path), 'pocket/alphafill_8A')
